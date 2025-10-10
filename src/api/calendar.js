d19984b6b020a4b8ad1a87e705b5399a071d6fba
DDehC <ali.hass@live.se>
2025-10-10 11:35:05 +0200

    Delete src directory

--- src/api/calendar.js
+++
@@ -1,9 +0,0 @@
-const API = "/api";
-
-export async function fetchCalendar(startYYYYMMDD, endYYYYMMDD) {
-  const q = new URLSearchParams({ start: startYYYYMMDD, end: endYYYYMMDD });
-  const res = await fetch(`${API}/req/calendar?${q.toString()}`, { credentials: "include" });
-  if (!res.ok) throw new Error(`HTTP ${res.status}`);
-  const body = await res.json();
-  return body.items || [];
-}
