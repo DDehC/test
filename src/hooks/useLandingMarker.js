38c11acb4d63064bd119a9f45cb4a570a6dd7aba
DDehC <ali.hass@live.se>
2025-10-10 14:54:34 +0200

    Delete src directory

--- src/hooks/useLandingMarker.js
+++
@@ -1,6 +0,0 @@
-import { useEffect } from "react";
-import { markLanding } from "../utils/landing";
-
-export default function useLandingMarker(path) {
-  useEffect(() => { markLanding(path); }, [path]);
-}
