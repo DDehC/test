eed34e5a4b344698c45c643d84fb0c5ca8ec3988
DDehC <ali.hass@live.se>
2025-10-15 15:48:46 +0200

    Delete src directory

--- src/utils/logout.js
+++
@@ -1,9 +0,0 @@
-// frontend/src/utils/logout.js
-import { logoutUser } from "../api/auth";
-import { clearRole } from "./landing";
-
-export async function doLogout() {
-  try { await logoutUser(); } catch {}
-  clearRole();
-  window.location.replace("/login");
-}
