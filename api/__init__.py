3084710c9c086e3267b62cea9394f4df0d03e46f
DDehC <ali.hass@live.se>
2025-10-15 15:48:21 +0200

    Delete api directory

--- api/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-api_bp = Blueprint("api", __name__, url_prefix="/api")
-admin_bp = Blueprint("admin", __name__, url_prefix="/admin")
-email_bp = Blueprint("email", __name__, url_prefix="/email")
-profile_bp = Blueprint("profile", __name__, url_prefix="/profile")
